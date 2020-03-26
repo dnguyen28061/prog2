fa34c457111f33d7b3759fab5096b097fd01fe10
Daniel <danielnguyen@college.harvard.edu>
2020-03-26 01:04:35 -0700

    finished padding

--- strassen.cpp
+++ strassen.cpp
@@ -4,149 +4,246 @@
 #include <fstream> 
 #include <string>
 #include<chrono> 
+#include<math.h> 
 
 struct Matrix{
     public: 
     int* firstNum; 
     int rowLength; 
-    int matrixLength; 
+    int matrixLength;  
+    // point to this dummy element when referring to an entry in padding 
+    int* zeroForPadding; 
+    // the start of padding in the x and y direction of the matrix  
+    int startOfHorizontalPadding; 
+    int startOfVerticalPadding; 
+    // cached pointers to the ABCD submatrices of this larger matrix. 
+    Matrix* blocks[4] = {nullptr, nullptr, nullptr, nullptr};
+
     Matrix(int* numbers = nullptr, int blockRows = 0, int matrixRows = 0){ 
         this->firstNum = numbers;
         this->rowLength = blockRows; 
+        this->startOfVerticalPadding = blockRows; 
+        this->startOfHorizontalPadding = blockRows; 
         this-> matrixLength = matrixRows; 
-    }
+        this-> zeroForPadding = new int; 
+        *zeroForPadding = 0; 
+    };
+    // ~Matrix(){
+    //     for(int i = 0; i < 4; ++i){ 
+    //         delete blocks[i]; 
+    //     }
+    // }
     
     // For a matrix of 
+    private: 
     int* elt(int n){ 
-        // std::cout << n << "\n";
-        assert((0 <= n) && (n < rowLength * rowLength)); 
         int offset = (n / rowLength * matrixLength) + (n % rowLength);
         return (firstNum + offset); 
+    };
+    public: 
+    int* rowCol(int i, int j){
+        // assert((0 <= i) && (0 <= j) && (i < rowLength) && (j < rowLength));
+        // if i, j are in the padding or all of the matrix is in the padding, then return 0. 
+        if (firstNum == zeroForPadding)
+        {
+            return zeroForPadding; 
+        }
+        else if (i >= startOfVerticalPadding || j >= startOfHorizontalPadding){ 
+            return zeroForPadding;
+        }
+        else{ 
+            return elt((rowLength * i) + j);
+        }
     }
-    Matrix block(int n){
+    private: 
+    int getPadding(int firstRow, int StartOfPadding){ 
+        // if padding is in the elements of the matrix. 
+        if (firstRow + (rowLength / 2) >= StartOfPadding){ 
+            return StartOfPadding - firstRow; 
+        } 
+        // if no elements are in the padding. 
+        else{ 
+            return rowLength / 2; 
+        }
+    }; 
+    public: 
+    Matrix* block(int n){
         assert((0 <= n) &&  (n < 4)); 
+        assert(rowLength % 2 == 0);
+        if (blocks[n] != nullptr){ 
+            return blocks[n]; 
+        }
         int* firstNumber; 
+        int startOfHorizPadding; 
+        int startOfVertPadding; 
+
         if (n == 0){ 
-            firstNumber = this->elt(0); 
+            firstNumber = this->rowCol(0, 0); 
+            startOfHorizPadding = getPadding(0, startOfHorizontalPadding); 
+            startOfVertPadding = getPadding(0, startOfVerticalPadding); 
         }
         else if (n == 1){ 
-            firstNumber = this->elt(rowLength / 2); 
+            firstNumber = this->rowCol(0, rowLength / 2); 
+            startOfHorizPadding = getPadding(rowLength/2, startOfHorizontalPadding); 
+            startOfVertPadding = getPadding(0, startOfVerticalPadding); 
         }
         else if (n == 2){ 
-            firstNumber = this->elt(rowLength * rowLength / 2); 
+            firstNumber = this->rowCol(rowLength / 2, 0); 
+            startOfHorizPadding = getPadding(0, startOfHorizontalPadding); 
+            startOfVertPadding = getPadding(rowLength/2, startOfVerticalPadding); 
         }
         // take the first num of second block and add rowLength / 2 
         else if (n == 3){ 
-            firstNumber = this->elt(rowLength * rowLength / 2) + rowLength / 2; 
+            firstNumber = this->rowCol(rowLength / 2, rowLength / 2);
+            startOfHorizPadding = getPadding(rowLength/2, startOfHorizontalPadding); 
+            startOfVertPadding = getPadding(rowLength/2, startOfVerticalPadding);  
         }
-        return Matrix(firstNumber, rowLength / 2, matrixLength); 
-    }
+        blocks[n] = new Matrix(firstNumber, rowLength / 2, matrixLength);
+        blocks[n]->startOfHorizontalPadding = startOfHorizPadding; 
+        blocks[n]->startOfVerticalPadding = startOfVertPadding; 
 
-    // returns element at ith row and jth column
-    int* rowCol(int i, int j){
-        assert((0 <= i) && (0 <= j) && (i < rowLength) && (j < rowLength));
-        return elt((rowLength * i) + j);
+        if(firstNumber == zeroForPadding){ 
+            blocks[n]->firstNum = blocks[n]->zeroForPadding; 
+        }
+
+        return blocks[n];
     }
 
     void printElts(){ 
-        for (int i = 0; i < rowLength * rowLength; ++i){ 
-            std::cout << *elt(i) << " ";
-            if (i % rowLength == rowLength - 1){
-                std::cout << "\n";
+        for (int i = 0; i < rowLength; ++i){ 
+            for (int j = 0; j < rowLength; ++j){
+                std::cout << *rowCol(i, j) << " ";
+                if (j == rowLength - 1){
+                    std::cout << "\n";
+                } 
             } 
         }
     }
+    void pad(){ 
+        assert(rowLength % 2 == 1); 
+        rowLength += 1; 
+    }
+    void unpad(){ 
+        assert(rowLength % 2 == 0); 
+        rowLength -= 1; 
+    }
+    
+    // void unpad(){ 
+    //     assert(rowLength % 2 == 0); 
+    //     for (int i = 0; i < rowLength; ++i){ 
+    //         std::cout << rowLength << "\n";
+    //         *(rowCol(i, rowLength - 1)) = *(paddedNums->rowCol(i, rowLength - 1)); 
+    //         *(rowCol(rowLength - 1, i)) = *(paddedNums->rowCol(rowLength - 1, i));
+    //     }
+    //     delete[] paddedNums->firstNum; 
+    //     delete paddedNums; 
+    //     rowLength -= 1; 
+    // }
 }; 
 
 //creates dynamically sized array of size n, initializes it to 0, and returns a pointer to that object 
-int* createNewArray(int n){ 
-    int* newArray = new int[n]; 
+
+int* createNewArray(int dimension, int d){ 
+    int* newArray = new int[dimension * dimension]; 
     int* newArrayPointer = newArray; 
-    for (int i = 0; i < n; ++i){ 
+
+    for (int i = 0; i < dimension * dimension; ++i){ 
         *newArrayPointer = 0; 
         newArrayPointer++; 
     }
     return newArray; 
 };
 // adds two matrices and stores them in resMatrix. Allocates new matrix if it does not exist. 
-Matrix addMatrix(Matrix m1, Matrix m2, bool isAddition = true, Matrix resultMatrix = Matrix(nullptr, 0, 0)){ 
-    if (resultMatrix.firstNum == nullptr){ 
-        int* resultArray = createNewArray(m1.matrixLength * m1.matrixLength); 
-        resultMatrix = Matrix(resultArray, m1.rowLength, m1.rowLength); 
+Matrix* addMatrix(Matrix* m1, Matrix* m2, bool isAddition = true, Matrix* resultMatrix = new Matrix(nullptr, 0, 0)){ 
+    if (resultMatrix->firstNum == nullptr){ 
+        int* resultArray = createNewArray(m1->matrixLength * m1->matrixLength); 
+        resultMatrix = new Matrix(resultArray, m1->rowLength, m1->rowLength); 
     }
-    for (int i = 0; i < m1.rowLength * m1.rowLength; ++i){ 
-        int sum; 
-        if (isAddition){ 
-            sum = *(m1.elt(i)) + *(m2.elt(i)); 
+    for (int i = 0; i < m1->rowLength; ++i){ 
+        for(int j = 0; j < m1->rowLength; ++j){ 
+            int sum;
+            if (isAddition){ 
+                sum = *(m1->rowCol(i, j)) + *(m2->rowCol(i, j)); 
+            }
+            else{ 
+                sum = *(m1->rowCol(i, j)) - *(m2->rowCol(i, j)); 
+            }
+            *resultMatrix->rowCol(i, j) = sum; 
         }
-        else{ 
-            sum = *(m1.elt(i)) - *(m2.elt(i)); 
-        }
-        *resultMatrix.elt(i) = sum; 
     }
     return resultMatrix;
 };
 
 // multiplies two matrices and stores the result in a new matrix 
-int* multConv(Matrix m1, Matrix m2); 
-Matrix multiplyStrassen(Matrix m1, Matrix m2, int n0){
-    assert(m1.rowLength = m2.rowLength); 
+int* multConv(Matrix* m1, Matrix* m2); 
+Matrix* multiplyStrassen(Matrix* m1, Matrix* m2, int n0){
+    assert(m1->rowLength = m2->rowLength); 
     // base case 
-    Matrix* matrix; 
     int* resultArray; 
-    if (m1.rowLength <= n0){ 
+    if (m1->rowLength <= n0){ 
         // multiply two matrices and store product in m1 
         resultArray = multConv(m1, m2); 
-        return Matrix(resultArray, m1.rowLength, m1.rowLength); 
+        return new Matrix(resultArray, m1->rowLength, m1->rowLength); 
     } 
     else{ 
-        resultArray = createNewArray(m1.matrixLength * m1.matrixLength); 
-        Matrix FH = addMatrix(m2.block(1), m2.block(3), false); 
-        Matrix AB = addMatrix(m1.block(0), m1.block(1)); 
-        Matrix CD = addMatrix(m1.block(2), m1.block(3)); 
-        Matrix GE = addMatrix(m2.block(2), m2.block(0), false); 
-        Matrix AD = addMatrix(m1.block(0), m1.block(3)); 
-        Matrix EH = addMatrix(m2.block(0), m2.block(3)); 
-        Matrix BD = addMatrix(m1.block(1), m1.block(3), false); 
-        Matrix GH = addMatrix(m2.block(2), m2.block(3)); 
-        Matrix AC = addMatrix(m1.block(0), m1.block(2), false); 
-        Matrix EF = addMatrix(m2.block(0), m2.block(1)); 
-        Matrix intermediates[7]; 
-        intermediates[0] = multiplyStrassen(m1.block(0), FH, n0); 
-        intermediates[1] = multiplyStrassen(AB, m2.block(3), n0);
-        intermediates[2] = multiplyStrassen(CD, m2.block(0), n0);
-        intermediates[3] = multiplyStrassen(m1.block(3), GE, n0);
+        resultArray = createNewArray(m1->matrixLength * m1->matrixLength); 
+        Matrix* resultMatrix = new Matrix(resultArray, m1->rowLength, m1->matrixLength); 
+        bool padded = false; 
+        if (m1->rowLength % 2 == 1){ 
+            m1->pad(); 
+            m2->pad(); 
+            resultMatrix->pad(); 
+        }
+
+        Matrix* FH = addMatrix(m2->block(1), m2->block(3), false); 
+        Matrix* AB = addMatrix(m1->block(0), m1->block(1)); 
+        Matrix* CD = addMatrix(m1->block(2), m1->block(3)); 
+        Matrix* GE = addMatrix(m2->block(2), m2->block(0), false); 
+        Matrix* AD = addMatrix(m1->block(0), m1->block(3)); 
+        Matrix* EH = addMatrix(m2->block(0), m2->block(3)); 
+        Matrix* BD = addMatrix(m1->block(1), m1->block(3), false); 
+        Matrix* GH = addMatrix(m2->block(2), m2->block(3)); 
+        Matrix* AC = addMatrix(m1->block(0), m1->block(2), false); 
+        Matrix* EF = addMatrix(m2->block(0), m2->block(1)); 
+        Matrix* intermediates[7]; 
+        intermediates[0] = multiplyStrassen(m1->block(0), FH, n0); 
+        intermediates[1] = multiplyStrassen(AB, m2->block(3), n0);
+        intermediates[2] = multiplyStrassen(CD, m2->block(0), n0);
+        intermediates[3] = multiplyStrassen(m1->block(3), GE, n0);
         intermediates[4] = multiplyStrassen(AD, EH, n0);
         intermediates[5] = multiplyStrassen(BD, GH, n0);
         intermediates[6] = multiplyStrassen(AC, EF, n0);
 
-        Matrix resultMatrix = Matrix(resultArray, m1.rowLength, m1.matrixLength); 
-
-        addMatrix(intermediates[4], intermediates[3], true, resultMatrix.block(0)); 
-        addMatrix(resultMatrix.block(0), intermediates[1], false, resultMatrix.block(0)); 
-        addMatrix(resultMatrix.block(0), intermediates[5], true, resultMatrix.block(0));
+        addMatrix(intermediates[4], intermediates[3], true, resultMatrix->block(0)); 
+        addMatrix(resultMatrix->block(0), intermediates[1], false, resultMatrix->block(0)); 
+        addMatrix(resultMatrix->block(0), intermediates[5], true, resultMatrix->block(0));
         
-        addMatrix(intermediates[0], intermediates[1], true, resultMatrix.block(1)); 
-        addMatrix(intermediates[2], intermediates[3], true, resultMatrix.block(2));
+        addMatrix(intermediates[0], intermediates[1], true, resultMatrix->block(1)); 
+        addMatrix(intermediates[2], intermediates[3], true, resultMatrix->block(2));
 
-        addMatrix(intermediates[4], intermediates[0], true, resultMatrix.block(3)); 
-        addMatrix(resultMatrix.block(3), intermediates[2], false, resultMatrix.block(3)); 
-        addMatrix(resultMatrix.block(3), intermediates[6], false, resultMatrix.block(3)); 
-
-        // 
-        Matrix matricesToDelete[17] = {FH, AB, CD, GE, AD, EH, BD, GH, AC, EF}; 
-        for(int j = 10; j < 17; ++j){ 
-            matricesToDelete[j] = intermediates[j-10]; 
+        addMatrix(intermediates[4], intermediates[0], true, resultMatrix->block(3)); 
+        addMatrix(resultMatrix->block(3), intermediates[2], false, resultMatrix->block(3)); 
+        addMatrix(resultMatrix->block(3), intermediates[6], false, resultMatrix->block(3)); 
+        if (padded){ 
+            m1->unpad(); 
+            m2->unpad(); 
+            resultMatrix->unpad(); 
         }
-        for (int i = 0; i < 17; ++i){ 
-            delete[] matricesToDelete[i].firstNum; 
-        }
+        // Matrix* matricesToDelete[17] = {FH, AB, CD, GE, AD, EH, BD, GH, AC, EF}; 
+        // for(int j = 10; j < 17; ++j){ 
+        //     matricesToDelete[j] = intermediates[j-10]; 
+        // }
+        // for (int i = 0; i < 17; ++i){ 
+        //     delete[] matricesToDelete[i]->firstNum; 
+        //     delete matricesToDelete[i]; 
+        // }
         return resultMatrix; 
     }
 }; 
 
+
 int* returnOneDArray(int** arr, int dimension){ 
-    int* flattenedArray = createNewArray(dimension*dimension); 
+    int* flattenedArray = createNewArray(dimension); 
     int currElt = 0; 
     for (int i = 0; i < dimension; ++i){ 
         for (int j = 0; j < dimension; ++j){ 
@@ -158,8 +255,8 @@ int* returnOneDArray(int** arr, int dimension){
 
 }
 // multiplies matrices using conventional method and returns product matrix
-int* multConv(Matrix m1, Matrix m2){
-    int N = m1.rowLength;
+int* multConv(Matrix* m1, Matrix* m2){
+    int N = m1->rowLength;
     int** resMatrix = 0;
     resMatrix = new int*[N];
     int i, j, k; 
@@ -168,11 +265,14 @@ int* multConv(Matrix m1, Matrix m2){
         for (j = 0; j < N; j++){ 
             resMatrix[i][j] = 0;
             for (k = 0; k < N; k++){
-                resMatrix[i][j] += *(m1.rowCol(i,k)) * *(m2.rowCol(k,j));
+                resMatrix[i][j] += *(m1->rowCol(i,k)) * *(m2->rowCol(k,j));
             }
         }
     }
     int* oneDimMatrix = returnOneDArray(resMatrix, N); 
+    // for (int i = 0; i < N * N; ++i){ 
+    //     std::cout << oneDimMatrix[i] << "\n"; 
+    // }
     for (int i = 0; i < N; ++i){ 
         delete[] resMatrix[i]; 
     }
@@ -189,6 +289,16 @@ int* multConv(Matrix m1, Matrix m2){
 //     }
 // }
 
+void readFileIntoArray(std::ifstream* file, int dimension, Matrix* matrix){ 
+    for (int i = 0; i < dimension; ++i){ 
+        for (int j = 0; j < dimension; ++j){ 
+            std::string numb; 
+            std::getline(*file, numb); 
+            *matrix->rowCol(i, j) = atoi(numb.c_str()); 
+        }
+    }
+}
+
 int main(int argc, char** argv){ 
     if (argc != 4) {
         throw std::invalid_argument("Usage: ./strassen 0 dimension inputfile");
@@ -196,27 +306,21 @@ int main(int argc, char** argv){
     int dimension = atoi(argv[2]); 
     int* matrix_1 = createNewArray(dimension * dimension); 
     int* matrix_2 = createNewArray(dimension * dimension); 
+    Matrix* matrixStruct = new Matrix(matrix_1, dimension, dimension);
+    Matrix* matrixStruct2 = new Matrix(matrix_2, dimension, dimension); 
     std::ifstream file(argv[3], std::ios::in);
-    for (int i = 0; i < dimension * dimension; ++i){ 
-        std::string numb; 
-        std::getline(file, numb); 
-        matrix_1[i] = atoi(numb.c_str()); 
-    }
-    for (int j = 0; j < dimension * dimension; ++j){ 
-        std::string numb; 
-        std::getline(file, numb); 
-        matrix_2[j] = atoi(numb.c_str()); 
-    }
-
-    Matrix matrixStruct = Matrix(matrix_1, dimension, dimension);
-    Matrix matrixStruct2 = Matrix(matrix_2, dimension, dimension); 
+    readFileIntoArray(&file, dimension, matrixStruct); 
+    readFileIntoArray(&file, dimension, matrixStruct2);
+    file.close(); 
     auto start = std::chrono::high_resolution_clock::now(); 
-    Matrix resMatrix = multiplyStrassen(matrixStruct, matrixStruct2, 8); 
+    Matrix* resMatrix = multiplyStrassen(matrixStruct, matrixStruct2, 1); 
     auto end = std::chrono::high_resolution_clock::now(); 
     int* convRes = multConv(matrixStruct, matrixStruct2);
     auto endConventional = std::chrono::high_resolution_clock::now(); 
     std::cout << "Time for Strassen: " << (std::chrono::duration_cast<std::chrono::microseconds>(end - start)).count() << "\n"; 
     std::cout << "Time for Conventional: " << (std::chrono::duration_cast<std::chrono::microseconds>(endConventional - end)).count() << "\n"; 
+    resMatrix->printElts(); 
+
     // std::cout << "\n";
     // // resMatrix.printElts();
     // for (int i = 0; i < matrixStruct.matrixLength; i++) 
@@ -225,10 +329,11 @@ int main(int argc, char** argv){
     //     std::cout << convRes[i][j] << " "; 
     //     std::cout << "\n"; 
     // }
-    resMatrix.printElts();
+
     delete[] matrix_1; 
     delete[] matrix_2;
-    delete[] resMatrix.firstNum; 
+    delete[] resMatrix->firstNum; 
     delete[] convRes;
-    file.close();  
+ 
+
 }
